e88e3e26bff34fdb96217aa45578785b9db3afa7
MelchyMelch <matnaruse@gmail.com>
2025-04-14 15:04:24 -0400

    Realizing model related functions can't be called async

--- lib/gui/main_gui.py
+++ lib/gui/main_gui.py
@@ -4,6 +4,6 @@ from lib.gui import eye_test
 
 # Adding the Reroll Button for testing
 debug_btn = eye_test.debug_reroll_btn
-
+result = eye_test.result
 # Running the UI
 ui.run()
--- lib/static_data/static_data.py
+++ lib/static_data/static_data.py
@@ -5,4 +5,4 @@ options = {"Up":"assets/up.png",
 
 vr_cmds = ['down', 'go', 'left', 'no', 'right', 'stop', 'up', 'yes']
 
-preset_test1 = ['up', 'down', 'left', 'right']
+preset_test1 = ['up', 'down', 'left', 'right', 'up', 'down', 'up']
