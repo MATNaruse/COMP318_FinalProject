527ab827a3101d856374aafbc0e4ad907c4f1ae4
MelchyMelch <matnaruse@gmail.com>
2025-04-13 23:15:16 -0400

    Imported simple_audio TensorFlow example snippets

---
+++ lib/external_resources/realtime_voice_command_recognition.py
@@ -0,0 +1,6 @@
+"""
+    Realtime Voice Command Recognition
+        https://www.youtube.com/watch?v=m-JzldXm9bQ
+        https://github.com/AssemblyAI-Community/realtime-voice-command-recognition
+    Using Specific Snippets for our Use Case
+"""
---
+++ lib/external_resources/simple_audio_graphs.py
@@ -0,0 +1,8 @@
+"""
+    Simple Audio Recognition Example from TensorFlow
+        https://www.tensorflow.org/tutorials/audio/simple_audio
+    Using Specific Snippets for our use case
+"""
+
+# TODO: Will Implement Graphs later for demo/presentation purposes (?)
+# MN - I just wanted them out of the way for now while I troubleshooted the model creation
